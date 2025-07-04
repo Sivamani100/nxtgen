704eeecda8930536b3a03dab87f5bca163799820
Sivamani100 <animations9901@gmail.com>
2025-07-04 17:16:57 +0530

    Update Predictor.tsx

--- src/pages/Predictor.tsx
+++ src/pages/Predictor.tsx
@@ -174,11 +174,7 @@ const Predictor = () => {
     <div className="min-h-screen bg-gray-50 pb-20 lg:pb-0">
       <div className="max-w-4xl mx-auto p-4 lg:p-6">
         {/* Header */}
-        <div className="mb-6 lg:mb-8 text-center lg:text-left">
-          <h1 className="text-2xl lg:text-3xl font-bold text-gray-900 mb-2">
-            College & Rank Predictor
-          </h1>
-        </div>
+        
 
         {/* Tabs */}
         <Tabs defaultValue="college-predictor" className="w-full">
